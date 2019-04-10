34e23a8620fc789a6a6e16ec9530eda9cce337c9
widoc84 <sysadmin84@gmail.com>
2019-04-11 01:29:16 +0400

    Задание 26 загрузка списка проектов через soup

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,5 +1,6 @@
 from suds.client import Client
 from suds import WebFault
+from model.proj import Proj
 
 class SoapH:
 
@@ -14,7 +15,17 @@ class SoapH:
         except WebFault:
             return False
 
-    def get_project(self,username):
+    def get_project(self):
         client = Client("http://localhost/mantisbt/api/soap/mantisconnect.php?wsdl")
-#        project = client.service.mc_enum(username) client.
-        return project
+        test = client.service.mc_projects_get_user_accessible('administrator','root')
+        list = []
+        for i in test:
+            id = i.id
+            name = i.name
+            description = i.description
+            tmp_list = Proj(id=id, name=name, description=description)
+            list.append(tmp_list)
+        return list
+
+    def add(self,proj):
+        client = Client("http://localhost/mantisbt/api/soap/mantisconnect.php?wsdl")
--- test/test_project_soap.py
+++ test/test_project_soap.py
@@ -1,15 +1,25 @@
 import random
 
+import random
+from model.proj import Proj
+
 def test_add_project(app):
-    old_proj = app.projects.get_proj()
+    wd = app.wd
+    wd.get(app.base_url)
+    app.session.login('administrator', 'root')
+    old_proj = app.soap.get_project()
     name = app.projects.create_type(6)
     des = app.projects.create_type(10)
-    app.projects.add_proj(name,des)
-    new_proj = app.projects.get_proj()
-    assert len(old_proj) + 1 == len(new_proj)
+    tmp = app.projects.add_proj(name,des)
+    old_proj.append(tmp)
+    new_proj = app.soap.get_project()
+    assert sorted(old_proj, key=Proj.id_or_nmx) == sorted(new_proj, key=Proj.id_or_nmx)
 
 def test_del_project(app):
-    old_proj = app.soap.get_project('administrator')
+    wd = app.wd
+    wd.get(app.base_url)
+    app.session.login('administrator', 'root')
+    old_proj = app.soap.get_project()
     if len(old_proj) < 1:
         name = app.projects.create_type(6)
         des = app.projects.create_type(10)
@@ -17,5 +27,6 @@ def test_del_project(app):
         old_proj = app.projects.get_proj()
     name =  random.choice(old_proj)
     app.projects.del_proj(name)
-    new_proj = app.projects.get_proj()
-    assert len(old_proj) - 1 == len(new_proj)
+    old_proj.remove(name)
+    new_proj = app.soap.get_project()
+    assert sorted(old_proj, key=Proj.id_or_nmx) == sorted(new_proj, key=Proj.id_or_nmx)
